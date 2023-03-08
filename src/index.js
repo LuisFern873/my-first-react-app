07abd712cbe862eac0abf54b5391efbdf418b6f8
LuisFern873 <luis.mendez.l@utec.edu.pe>
2023-03-08 12:39:06 -0500

    Hello, Luis!

--- src/index.js
+++ src/index.js
@@ -1,17 +1,32 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from 'react'
+import ReactDOM from 'react-dom/client'
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById('root'))
+
+const user = new Map([
+    ['name', 'Luis'],
+    ['surname', 'Méndez'],
+    ['email', 'luis.mendez.l@utec.edu.pe'],
+    ['age', 19]
+])
+
+// Components and JSX
+function Greeting(props){
+    return <h1>Hello, {props.name}!</h1>
+}
+
+function Information(){
+    return <div>
+        <h3>{user.get('surname')}</h3>
+        <h3>{user.get('email')}</h3>
+        <h3>{user.get('age')}</h3>
+    </div>
+}
+
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+    // Fragment and Self Closing Tags
+    <> 
+        <Greeting name = {user.get('name')} /> 
+        <Information /> 
+    </>
+)
